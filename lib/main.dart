f142f9329e4d66cabf025cac72ca2d23e820436d
Matthieu <matthieunadeau96@gmail.com>
2020-01-20 10:48:54 -0500

    Added an example of a stateful widget and used setState to change the question text

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen!');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
-      'What\'s your favoirte animal?'
+      'What\'s your favorite animal?'
     ];
 
     return MaterialApp(
@@ -23,7 +36,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
